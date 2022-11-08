b23ed5a721cc9f5bc1a024135e6cf07974ff5753
prashant <prashant.aghara2111@gmail.com>
2022-11-08 12:42:29 +0530

    Completed all Pages

--- react-dashboard/src/pages/Employee.jsx
+++ react-dashboard/src/pages/Employee.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Edit,
+  Toolbar,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, contextMenuItems, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employee = () => {
-  return <div>Employee</div>;
+  const editing = { allowDeleting: true, allowEditing: true };
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={["Search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
-
 export default Employee;
